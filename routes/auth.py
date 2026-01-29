6bde8799541e49af2e3d5f7265617d60dabcf094
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-29 15:17:31 +0500

    add(signin_user): this has been added

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,5 @@
 import bcrypt
-from pydatic_schemas.user_create import UserCreate
+from pydatic_schemas.user_create import LoginUser, UserCreate
 from database import get_db
 from models.user import User
 from fastapi import HTTPException,Depends
@@ -26,6 +26,21 @@ def signup_user(user:UserCreate, db:Session = Depends(get_db)):
     return new_user
 
 
-@router.post("/login")
-def signin_user():
-    pass
+@router.post("/login",)
+def signin_user(user:LoginUser,db:Session = Depends(get_db)):
+    # FIND USER!
+    user_db = db.query(User).filter(User.email == user.email).first() 
+
+    # IF NO USER EROR TROW!
+    if not user_db:
+        raise HTTPException(404,"User not found!")
+
+    # IF USER VALIDATE CREDENTIALS!
+    compare_pass = bcrypt.checkpw(user.password.encode(), user_db.password)
+
+    # IF NO THROW ERROR!
+    if not compare_pass:
+        raise HTTPException(404,"Invalid credentials!")
+
+    # ELSE RETURN THE USER!
+    return user_db
